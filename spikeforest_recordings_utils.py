22dbe040b3a154ad554da327eb1f4ac6c04c84e5
Michael Clancy <mjclancy@protonmail.ch>
2021-07-29 11:17:26 +0100

    Usage of new utilites for retrieving recordings can be found in spikeforest_recordings.ipynb

--- spikeforest_recordings_utils.py
+++ spikeforest_recordings_utils.py
@@ -15,7 +15,7 @@ def _get_sf_metadata(study_set_name: Optional[str] = None,
     if recording_name is not None and study_name is None:
         raise ValueError("If a recording name is provided, a substudy must also be provided.")
 
-    metadata = kc.load_json(STUDY_SET_METADATA_URL)
+    metadata = kc.load_json(STUDY_SET_METADATA_URL)['StudySets']
     if study_set_name is not None:
         metadata = [m for m in metadata if m['name'].lower() == study_set_name.lower()][0]
     if study_name is not None:
@@ -55,7 +55,7 @@ class RecordingSet:
         self.recording_uri = recording_uri
         self.sorting_true_uri = sorting_true_uri
 
-    def get_recording(self, download: bool) -> sv.LabboxEphysRecordingExtractor:
+    def get_recording(self, download: Optional[bool] = False) -> sv.LabboxEphysRecordingExtractor:
         return sv.LabboxEphysRecordingExtractor(self.recording_uri, download=download)
 
     def get_ground_truth(self) -> sv.LabboxEphysSortingExtractor:
@@ -82,7 +82,7 @@ class Study:
     def get_recording_names(self):
         return [recording['name'] for recording in self._recordings]
 
-    def get_recording(self, name) -> RecordingSet:
+    def get_recording_set(self, name) -> RecordingSet:
         return [RecordingSet(name=recording['name'], study_name=recording['studyName'],
                              study_set_name=recording['studySetName'], sample_rate_hz=recording['sampleRateHz'],
                              num_channels=recording['numChannels'], duration_sec=recording['durationSec'],
@@ -115,37 +115,3 @@ class StudySet:
     def get_study(self, name: str) -> Study:
         return [Study(name=study['name'], study_set_name=study['studySetName'], recordings=study['recordings'],
                       self_reference=study['self_reference']) for study in self._studies if study['name'] == name][0]
-
-
-# def get_kachery_recording_gt_path_pairs_for_study_set(
-#         study_set_name: str,
-#         sub_study_name: str = None,
-#         recording_name: str = None
-# ):
-#     if recording_name is not None and sub_study_name is None:
-#         raise ValueError("If a recording name is provided, a substudy must also be provided.")
-#
-#     recording_gt_pairs = {}
-#     for sub_study_set in (spikeforest_recordings / study_set_name).iterdir():
-#         if sub_study_set.is_dir() and (sub_study_name is None or sub_study_name == sub_study_set.stem):
-#             for gt_metadata in sub_study_set.glob('*.firings_true.json'):
-#                 recording_gt_pair_name = gt_metadata.name.split('.')[0]
-#                 if recording_name is None or recording_gt_pair_name == recording_name:
-#                     recording_gt_pairs[sub_study_set.stem + '_' + recording_gt_pair_name] = \
-#                         (sub_study_set / (recording_gt_pair_name + '.json'), gt_metadata)
-#     return recording_gt_pairs
-#
-#
-# def get_labbox_ephys_extractors(
-#         study_set_name: str,
-#         sub_study_name: str = None,
-#         recording_name: str = None,
-#         download_full_recording: bool = False
-# ):
-#     recording_gt_path_pairs = get_kachery_recording_gt_path_pairs_for_study_set(
-#         study_set_name, sub_study_name, recording_name)
-#
-#     return {k: (
-#         sv.LabboxEphysRecordingExtractor(str(v[0]), download=download_full_recording),
-#         sv.LabboxEphysSortingExtractor(str(v[1])))
-#         for k, v in recording_gt_path_pairs.items()}
