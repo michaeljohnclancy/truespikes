34250dde12a44aa9d24505f900f1215ef770ba1a
Michael Clancy <mjclancy@protonmail.ch>
2021-07-29 11:01:03 +0100

    Can read in and use study sets, studies, or recordings,abstracting away all kachery urls.

--- spikeforest_recordings_utils.py
+++ spikeforest_recordings_utils.py
@@ -1,19 +1,49 @@
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 import sortingview as sv
 import kachery_client as kc
-import git
-
-spikeforest_recordings = Path('spikeforest_recordings/recordings')
-
-git.cmd.Git(spikeforest_recordings).pull()
 
 STUDY_SET_METADATA_URL = 'sha1://f728d5bf1118a8c6e2dfee7c99efb0256246d1d3/studysets.json'
 
 
+def _get_sf_metadata(study_set_name: Optional[str] = None,
+                     study_name: Optional[str] = None,
+                     recording_name: Optional[str] = None):
+    if study_set_name is None:
+        raise ValueError("Must provide a study_set name!")
+    if recording_name is not None and study_name is None:
+        raise ValueError("If a recording name is provided, a substudy must also be provided.")
+
+    metadata = kc.load_json(STUDY_SET_METADATA_URL)
+    if study_set_name is not None:
+        metadata = [m for m in metadata if m['name'].lower() == study_set_name.lower()][0]
+    if study_name is not None:
+        metadata = [m for m in metadata['studies'] if m['name'].lower() == study_name.lower()][0]
+    if recording_name is not None:
+        metadata = [m for m in metadata['recordings'] if m['name'].lower() == recording_name.lower()][0]
+
+    return metadata
+
+
 class RecordingSet:
-    def __init__(self, name: str, study_name: str, study_set_name: str, sample_rate_hz: int, num_channels: int, duration_sec: float, num_true_units: int, spike_sign: int, recording_uri: str, sorting_true_uri: str):
+    @staticmethod
+    def load(study_set_name: str, study_name: str, recording_name: str):
+        recording_set_metadata = _get_sf_metadata(study_set_name, study_name, recording_name)
+        return RecordingSet(name=recording_set_metadata['name'],
+                            study_name=recording_set_metadata['studyName'],
+                            study_set_name=recording_set_metadata['studySetName'],
+                            sample_rate_hz=recording_set_metadata['sampleRateHz'],
+                            num_channels=recording_set_metadata['numChannels'],
+                            duration_sec=recording_set_metadata['durationSec'],
+                            num_true_units=recording_set_metadata['numTrueUnits'],
+                            spike_sign=recording_set_metadata['spikeSign'],
+                            recording_uri=recording_set_metadata['recordingUri'],
+                            sorting_true_uri=recording_set_metadata['sortingTrueUri']
+                            )
+
+    def __init__(self, name: str, study_name: str, study_set_name: str, sample_rate_hz: int, num_channels: int,
+                 duration_sec: float, num_true_units: int, spike_sign: int, recording_uri: str, sorting_true_uri: str):
         self.name = name
         self.study_name = study_name
         self.study_set_name = study_set_name
@@ -33,6 +63,16 @@ class RecordingSet:
 
 
 class Study:
+
+    @staticmethod
+    def load(study_set_name: str, study_name: str):
+        study_metadata = _get_sf_metadata(study_set_name, study_name)
+        return Study(name=study_metadata['name'],
+                     recordings=study_metadata['recordings'],
+                     study_set_name=study_metadata['studySetName'],
+                     self_reference=study_metadata['self_reference']
+                     )
+
     def __init__(self, name: str, study_set_name: str, recordings: List[Dict], self_reference: str):
         self._name = name
         self._recordings = recordings
@@ -44,10 +84,10 @@ class Study:
 
     def get_recording(self, name) -> RecordingSet:
         return [RecordingSet(name=recording['name'], study_name=recording['studyName'],
-                          study_set_name=recording['studySetName'], sample_rate_hz=recording['sampleRateHz'],
-                          num_channels=recording['numChannels'], duration_sec=recording['durationSec'],
-                          num_true_units=recording['numTrueUnits'], spike_sign=recording['spikeSign'],
-                          recording_uri=recording['recordingUri'], sorting_true_uri=recording['sortingTrueUri'])
+                             study_set_name=recording['studySetName'], sample_rate_hz=recording['sampleRateHz'],
+                             num_channels=recording['numChannels'], duration_sec=recording['durationSec'],
+                             num_true_units=recording['numTrueUnits'], spike_sign=recording['spikeSign'],
+                             recording_uri=recording['recordingUri'], sorting_true_uri=recording['sortingTrueUri'])
                 for recording in self._recordings if recording['name'].lower() == name.lower()][0]
 
 
@@ -55,6 +95,14 @@ class StudySet:
     _studies: List[Dict]
     description: str
     self_reference: str
+
+    @staticmethod
+    def load(study_set_name: str):
+        study_set_metadata = _get_sf_metadata(study_set_name=study_set_name)
+        return StudySet(name=study_set_metadata['name'],
+                        studies=study_set_metadata['studies'],
+                        self_reference=study_set_metadata['self_reference']
+                        )
 
     def __init__(self, name: str, studies: List[Dict], self_reference: str):
         self.name = name
@@ -64,48 +112,40 @@ class StudySet:
     def get_study_names(self) -> List[str]:
         return [study['name'] for study in self._studies]
 
-    def get_study(self, name:str) -> Study:
-        return [Study(name=study['name'], study_set_name=study['studySetName'], recordings=study['recordings'], self_reference=study['self_reference']) for study in self._studies if study['name'] == name][0]
+    def get_study(self, name: str) -> Study:
+        return [Study(name=study['name'], study_set_name=study['studySetName'], recordings=study['recordings'],
+                      self_reference=study['self_reference']) for study in self._studies if study['name'] == name][0]
 
 
-def get_study_set_metadata():
-    return kc.load_json(STUDY_SET_METADATA_URL)
-
-
-def get_study_set_info(study_set_name: str):
-    return [study_set_metadata for study_set_metadata in get_study_set_metadata()
-            if study_set_metadata['name'].lower() == study_set_name.lower()][0]
-
-
-def get_kachery_recording_gt_path_pairs_for_study_set(
-        study_set_name: str,
-        sub_study_name: str = None,
-        recording_name: str = None
-):
-    if recording_name is not None and sub_study_name is None:
-        raise ValueError("If a recording name is provided, a substudy must also be provided.")
-
-    recording_gt_pairs = {}
-    for sub_study_set in (spikeforest_recordings / study_set_name).iterdir():
-        if sub_study_set.is_dir() and (sub_study_name is None or sub_study_name == sub_study_set.stem):
-            for gt_metadata in sub_study_set.glob('*.firings_true.json'):
-                recording_gt_pair_name = gt_metadata.name.split('.')[0]
-                if recording_name is None or recording_gt_pair_name == recording_name:
-                    recording_gt_pairs[sub_study_set.stem + '_' + recording_gt_pair_name] =\
-                        (sub_study_set/(recording_gt_pair_name + '.json'), gt_metadata)
-    return recording_gt_pairs
-
-
-def get_labbox_ephys_extractors(
-        study_set_name: str,
-        sub_study_name: str = None,
-        recording_name: str = None,
-        download_full_recording: bool = False
-):
-    recording_gt_path_pairs = get_kachery_recording_gt_path_pairs_for_study_set(
-        study_set_name, sub_study_name, recording_name)
-
-    return {k: (
-        sv.LabboxEphysRecordingExtractor(str(v[0]), download=download_full_recording),
-        sv.LabboxEphysSortingExtractor(str(v[1])))
-            for k, v in recording_gt_path_pairs.items()}
+# def get_kachery_recording_gt_path_pairs_for_study_set(
+#         study_set_name: str,
+#         sub_study_name: str = None,
+#         recording_name: str = None
+# ):
+#     if recording_name is not None and sub_study_name is None:
+#         raise ValueError("If a recording name is provided, a substudy must also be provided.")
+#
+#     recording_gt_pairs = {}
+#     for sub_study_set in (spikeforest_recordings / study_set_name).iterdir():
+#         if sub_study_set.is_dir() and (sub_study_name is None or sub_study_name == sub_study_set.stem):
+#             for gt_metadata in sub_study_set.glob('*.firings_true.json'):
+#                 recording_gt_pair_name = gt_metadata.name.split('.')[0]
+#                 if recording_name is None or recording_gt_pair_name == recording_name:
+#                     recording_gt_pairs[sub_study_set.stem + '_' + recording_gt_pair_name] = \
+#                         (sub_study_set / (recording_gt_pair_name + '.json'), gt_metadata)
+#     return recording_gt_pairs
+#
+#
+# def get_labbox_ephys_extractors(
+#         study_set_name: str,
+#         sub_study_name: str = None,
+#         recording_name: str = None,
+#         download_full_recording: bool = False
+# ):
+#     recording_gt_path_pairs = get_kachery_recording_gt_path_pairs_for_study_set(
+#         study_set_name, sub_study_name, recording_name)
+#
+#     return {k: (
+#         sv.LabboxEphysRecordingExtractor(str(v[0]), download=download_full_recording),
+#         sv.LabboxEphysSortingExtractor(str(v[1])))
+#         for k, v in recording_gt_path_pairs.items()}
